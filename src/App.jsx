1f610b58748012cbe12cdfe0526904d0922d67dc
mguittari <mattias.guittari@gmail.com>
2025-03-12 23:39:27 +0100

    scss initialized

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,12 @@ function App() {
 	return (
 		<>
 			<main>
-				<div className="top-bloc">Composant 1</div>
-				<div className="header-bloc">Composant 2</div>
+				<div className="top-bloc">
+					<p>Composant 1</p>
+				</div>
+				<div className="header-bloc">
+					<h1>Composant 2</h1>
+				</div>
 				<div className="education-bloc">Composant 3</div>
 			</main>
 		</>
