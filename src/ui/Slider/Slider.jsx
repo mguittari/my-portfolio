5d0079d217fdc12a12209c72f246e6fe0fcadd4a
mguittari <mattias.guittari@gmail.com>
2025-04-21 20:22:54 +0200

    carousel mobile and new pictures

--- src/ui/Slider/Slider.jsx
+++ src/ui/Slider/Slider.jsx
@@ -1,39 +1,23 @@
-import { useState } from "react";
-import ArrowButton from "../ArrowButton/ArrowButton";
 import SlideItem from "../SlideItem/SlideItem";
 import styles from "./slider.module.scss";
 import data from "../../myProjects.json";
 
 export default function Slider() {
-	const [currentIndex, setCurrentIndex] = useState(0);
-	const nextSlide = () => {
-		setCurrentIndex((prevIndex) => (prevIndex + 1) % data.length);
-	};
-	const prevSlide = () => {
-		setCurrentIndex((prevIndex) =>
-			prevIndex === 0 ? data.length - 1 : prevIndex - 1,
-		);
-	};
-	console.log(data[currentIndex].github_url);
-
 	return (
-		<>
-			<div className={styles["slider-container"]}>
-				<SlideItem
-					title={data[currentIndex].title}
-					image={data[currentIndex].image}
-					description={data[currentIndex].description}
-					githubUrl={data[currentIndex].github_url}
-					appUrl={data[currentIndex].app_url}
-					index={currentIndex}
-				/>
-				{currentIndex !== 0 && (
-					<ArrowButton direction="left" onClick={prevSlide} />
-				)}
-				{currentIndex + 1 !== data.length && (
-					<ArrowButton direction="right" onClick={nextSlide} />
-				)}
+		<div className={styles["slider-container"]}>
+			<div className={styles["slider-track"]}>
+				{data.map((project, index) => (
+					<SlideItem
+						key={index}
+						title={project.title}
+						image={project.image}
+						description={project.description}
+						githubUrl={project.github_url}
+						appUrl={project.app_url}
+						index={index}
+					/>
+				))}
 			</div>
-		</>
+		</div>
 	);
 }
